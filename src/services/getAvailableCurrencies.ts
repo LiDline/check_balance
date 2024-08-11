58eebfc5498054e73a04ecd5c3a94653adc2c71e
LiDline <artyom.zhevnerenko@yandex.ru>
2024-08-11 15:25:41 +0300

    Api. Добавлена поддержка sqlite

--- src/services/getAvailableCurrencies.ts
+++ src/services/getAvailableCurrencies.ts
@@ -2,7 +2,34 @@ import { CurrencyAddress, sequelize } from '../database/db';
 import { GroupedCurrenciesResponse } from '../interfaces/interfaces.getAvailableCurrencies';
 
 export default async function getAvailableCurrencies() {
-  const groupedCurrencies = (await CurrencyAddress.findAll({
+  const postgree = !!process.env.URL_POSTGREESQL;
+
+  let groupedCurrencies: GroupedCurrenciesResponse;
+
+  if (postgree) {
+    groupedCurrencies = (await CurrencyAddress.findAll({
+      attributes: [
+        'currency',
+        [
+          sequelize.fn('STRING_AGG', sequelize.col('address'), ','),
+          'addresses',
+        ],
+      ],
+      group: ['currency'],
+      raw: true,
+    })) as unknown as GroupedCurrenciesResponse;
+  } else {
+    groupedCurrencies = (await CurrencyAddress.findAll({
+      attributes: [
+        'currency',
+        [sequelize.fn('GROUP_CONCAT', sequelize.col('address')), 'addresses'],
+      ],
+      group: ['currency'],
+      raw: true,
+    })) as unknown as GroupedCurrenciesResponse;
+  }
+
+  (await CurrencyAddress.findAll({
     attributes: [
       'currency',
       [sequelize.fn('STRING_AGG', sequelize.col('address'), ','), 'addresses'],
@@ -11,10 +38,12 @@ export default async function getAvailableCurrencies() {
     raw: true,
   })) as unknown as GroupedCurrenciesResponse;
 
+  console.log(groupedCurrencies);
+
   const result = groupedCurrencies.map((item) => ({
     currency: item.currency,
     addresses: item.addresses ? item.addresses.split(',') : [],
   }));
 
-  return result;
+  return groupedCurrencies;
 }
