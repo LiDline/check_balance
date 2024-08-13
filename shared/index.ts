9f79e865ac27e2afaa4b79c7e5c7ea65770df83a
LiDline <artyom.zhevnerenko@yandex.ru>
2024-08-13 11:15:22 +0300

    Api. Приведение к общим типам

--- shared/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/CONST';
-export * from './src/interfaces/index';
-export * from './src/validation/index';
---
+++ shared/src/index.ts
@@ -0,0 +1,3 @@
+export * from './CONST';
+export * from './interfaces/index';
+export * from './validation/index';
